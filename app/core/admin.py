1d428bac19949bcacc1e31081b7fafcde0cf08e5
juliobriones <julesc00@hotmail.com>
2020-10-11 03:25:26 -0500

    Updated admin to support custom user model.

--- app/core/admin.py
+++ app/core/admin.py
@@ -20,7 +20,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": ("email", "password1", "password")
+            "fields": ("email", "password1", "password2")
         }),
     )
 
